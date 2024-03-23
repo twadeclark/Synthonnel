d67b02133cc1fa0597acfd2d6e6aeb19a16a184e
twadeclark <115502590+twadeclark@users.noreply.github.com>
2024-03-22 21:21:09 -0700

    added prompt textarea loads items from file delte items in ui

--- main.py
+++ main.py
@@ -29,36 +29,18 @@ json_file_path = 'items.json'
 
 @app.post("/save-items")
 def save_items(items: List[Item]):
-    with open(json_file_path, 'w') as file:
+    with open(json_file_path, 'w', encoding='utf-8') as file:
         # Convert each Item instance in the list to a dict and serialize the list of dicts
         items_dict = [item.dict() for item in items]
         json.dump(items_dict, file, indent=4)
     return {"message": "Items list updated successfully"}
 
-
-@app.get("/read_items")
-def read_items():
+@app.get("/get-items")
+def get_items():
     if os.path.exists(json_file_path) and os.path.getsize(json_file_path) > 0:
-        with open(json_file_path, 'r') as file:
+        with open(json_file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
     else:
         return []
-
-# def write_items_to_json(items: List[dict]):
-#     with open(json_file_path, 'w') as file:
-#         json.dump(items, file, indent=4)
-
-# @app.post("/add-item")
-# def add_item(item: Item):
-#     items = read_items_from_json()
-#     items.append(item.dict())
-#     write_items_to_json(items)
-#     return {"message": "Item added successfully"}
-
-# # Example endpoint to get all items
-# @app.get("/items")
-# def get_items():
-#     return read_items_from_json()
-
 
 app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
