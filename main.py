160cf97307e1a852da61a5c0952867f26a240758
twadeclark <115502590+twadeclark@users.noreply.github.com>
2024-03-29 16:11:47 -0700

    preformatted font switch

--- main.py
+++ main.py
@@ -31,23 +31,24 @@ class Item(BaseModel):
 async def stream_llm_response(websocket: WebSocket):
     await websocket.accept()
     data = await websocket.receive_text()
-    itemData = json.loads(data)
+    item_data = json.loads(data)
 
-    dump_text = ""
-    for key, value in itemData.items():
-        dump_text += f"{key}: {value}\n"
+    dump_text = "That is an excellent question. I'm glad you asked. Here is the information you requested. "
+    for key, value in item_data.items():
+        dump_text += f"You sent the key {key} and the value is {value}. "
 
-    # lorem = " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus scelerisque eleifend donec pretium. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Cursus risus at ultrices mi tempus imperdiet. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Quam adipiscing vitae proin sagittis nisl. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. Vivamus arcu felis bibendum ut tristique et egestas quis. Amet volutpat consequat mauris nunc. Ut pharetra sit amet aliquam id diam maecenas ultricies. Nisl vel pretium lectus quam id leo in vitae. At ultrices mi tempus imperdiet."
+    dump_text += "Do you know my good friends lorem and ipsum? Here is their story."
     lorem = " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus scelerisque eleifend donec pretium. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Cursus risus at ultrices mi tempus imperdiet. Varius vel pharetra vel turpis nunc eget lorem dolor sed."
     temp_text = dump_text + lorem
     split_text = temp_text.split()
     split_trunc = split_text[:random.randint( len(dump_text.split()), len(split_text) - 1)]
 
-    base_int = random.uniform(0.01, 0.1)
-    await asyncio.sleep(base_int + random.uniform(0.5, 2.0))
+    await asyncio.sleep(random.uniform(0.5, 2.0))
+
+    base_int = random.uniform(0.01, 0.3)
 
     for word in split_trunc[:len(split_trunc) - 1]:
-        await asyncio.sleep(base_int + random.uniform(0.01, 0.1))
+        await asyncio.sleep(base_int * random.uniform(0.01, 0.3))
         await websocket.send_text(word)
 
     await websocket.close()
