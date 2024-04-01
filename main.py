e2d99a1d59299c8c080fa4f41cf5dbf5b6daea92
twadeclark <115502590+twadeclark@users.noreply.github.com>
2024-04-01 01:28:03 -0700

    added UI for saved models adjusted error message

--- main.py
+++ main.py
@@ -33,23 +33,26 @@ async def stream_llm_response(websocket: WebSocket):
     data = await websocket.receive_text()
     item_data = json.loads(data)
 
-    dump_text = "That is an excellent question. I'm glad you asked. Here is the information you requested. "
-    for key, value in item_data.items():
-        dump_text += f"You sent the key {key} and the value is {value}. "
+    dump_text = []
+    dump_text.append("That is an excellent question. I'm glad you asked. Here is the information you requested.")
+    dump_text.append("Do you know my good friends lorem and ipsum? Here is their story.")
+    dump_text.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus scelerisque eleifend donec pretium. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Cursus risus at ultrices mi tempus imperdiet. Varius vel pharetra vel turpis nunc eget lorem dolor sed.")
 
-    dump_text += "Do you know my good friends lorem and ipsum? Here is their story."
-    lorem = " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus scelerisque eleifend donec pretium. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Cursus risus at ultrices mi tempus imperdiet. Varius vel pharetra vel turpis nunc eget lorem dolor sed."
-    temp_text = dump_text + lorem
-    split_text = temp_text.split()
-    split_trunc = split_text[:random.randint( len(dump_text.split()), len(split_text) - 1)]
+    for key, value in item_data.items():
+        dump_text.append(f"You sent the key {key} and the value is {value}.")
 
     await asyncio.sleep(random.uniform(0.5, 2.0))
 
     base_int = random.uniform(0.01, 0.3)
+    random.shuffle(dump_text)
+    dump_text = dump_text[random.randint(1, 4):]
+    temp_text = '\n\n'.join(dump_text)
 
-    for word in split_trunc[:len(split_trunc) - 1]:
+    for i in range(0, len(temp_text), 5):
+        chunk = temp_text[i:i+5]
+
         await asyncio.sleep(base_int * random.uniform(0.01, 0.3))
-        await websocket.send_text(word)
+        await websocket.send_text(chunk)
 
     await websocket.close()
 
