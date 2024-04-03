9bb90caa63148253cb1bea686e01cccec4887eab
twadeclark <115502590+twadeclark@users.noreply.github.com>
2024-04-03 02:24:28 -0700

    abstracting model handlers

--- function_wrapper.py
+++ function_wrapper.py
@@ -1,3 +1,6 @@
+import json
+import random
+from fastapi import WebSocket
 from pydantic import BaseModel
 
 
@@ -13,8 +16,52 @@ def add(x, y):
 def subtract(x, y):
     return "Not Implemented: subtract(x, y)" + x + y
 
-def default(x, y):
-    return "Not Implemented: default(x, y)" + x + y
+async def default(websocket: WebSocket):
+    await websocket.accept()
+    data = await websocket.receive_text()
+    item_data = json.loads(data)
+
+    dump_text = ""
+    for key, value in item_data.items():
+        dump_text += f"You sent the key '{key}' and the value is '{value}'.\n"
+
+    lorem = []
+
+    lorem.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n")
+    lorem.append("\nEu mi bibendum neque egestas congue. ")
+    lorem.append("Faucibus et molestie ac feugiat sed lectus vestibulum mattis. ")
+    lorem.append("Enim neque volutpat ac tincidunt.\n")
+    lorem.append("\nConvallis aenean et tortor at risus viverra adipiscing at. ")
+    lorem.append("In cursus turpis massa tincidunt dui ut ornare. ")
+    lorem.append("Eu mi bibendum neque egestas congue quisque.\n")
+    lorem.append("\nUrna porttitor rhoncus dolor purus. ")
+    lorem.append("Vitae elementum curabitur vitae nunc sed velit dignissim sodales. ")
+    lorem.append("Sed tempus urna et pharetra pharetra massa massa ultricies mi.\n")
+    lorem.append("\nSagittis nisl rhoncus mattis rhoncus. ")
+    lorem.append("Nibh cras pulvinar mattis nunc sed blandit libero volutpat. ")
+    lorem.append("Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla.\n")
+    lorem.append("\nDuis tristique sollicitudin nibh sit amet commodo nulla. ")
+    lorem.append("Vulputate enim nulla aliquet porttitor lacus luctus. ")
+    lorem.append("Mattis pellentesque id nibh tortor id aliquet.\n")
+    lorem.append("\nTurpis cursus in hac habitasse platea. ")
+    lorem.append("Semper risus in hendrerit gravida rutrum quisque. ")
+    lorem.append("Pulvinar neque laoreet suspendisse interdum.\n")
+    lorem.append("\nIn est ante in nibh mauris. ")
+
+    random.shuffle(lorem)
+
+    lorem_out = lorem[:random.randint(1, len(lorem))]
+    lorem_addendum = ''.join(lorem_out)
+    response_text = dump_text + lorem_addendum
+
+    for i in range(0, len(response_text), 5):
+        chunk = response_text[i:i+5]
+        # await asyncio.sleep(0.01) # 0.02 : 33 t/s .  0.01 : 66 t/s . 0.0 : 960 t/s . commented  : 1030 t / s
+        await websocket.send_text(chunk)
+
+    await websocket.close()
+
+    return "default must have worked."
 
 def lm_studio(x, y):
     return "Not Implemented: lm_studio(x, y)" + x + y
@@ -33,4 +80,3 @@ functions = {
 class FunctionInfo(BaseModel):
     friendly_name: str
     id: str
-
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from typing import List
 import json
-import random
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -29,54 +28,18 @@ class Item(BaseModel):
     parameters: str
     apiKey: str
 
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
+
 @app.websocket("/ws/stream-llm-response")
 async def stream_llm_response(websocket: WebSocket):
-    await websocket.accept()
-    data = await websocket.receive_text()
-    item_data = json.loads(data)
+    function_id = "default"
+    if function_id in function_wrapper.functions:
+        result = function_wrapper.functions[function_id].func(websocket)  # Call function by ID
+        await result
+        print(f"Result ({function_id}: {result}")
+    else:
+        print(f"Function with ID '{function_id}' not found.")
 
-    dump_text = ""
-    for key, value in item_data.items():
-        dump_text += f"You sent the key '{key}' and the value is '{value}'.\n"
-
-    lorem = []
-
-    lorem.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n")
-    lorem.append("\nEu mi bibendum neque egestas congue. ")
-    lorem.append("Faucibus et molestie ac feugiat sed lectus vestibulum mattis. ")
-    lorem.append("Enim neque volutpat ac tincidunt.\n")
-    lorem.append("\nConvallis aenean et tortor at risus viverra adipiscing at. ")
-    lorem.append("In cursus turpis massa tincidunt dui ut ornare. ")
-    lorem.append("Eu mi bibendum neque egestas congue quisque.\n")
-    lorem.append("\nUrna porttitor rhoncus dolor purus. ")
-    lorem.append("Vitae elementum curabitur vitae nunc sed velit dignissim sodales. ")
-    lorem.append("Sed tempus urna et pharetra pharetra massa massa ultricies mi.\n")
-    lorem.append("\nSagittis nisl rhoncus mattis rhoncus. ")
-    lorem.append("Nibh cras pulvinar mattis nunc sed blandit libero volutpat. ")
-    lorem.append("Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla.\n")
-    lorem.append("\nDuis tristique sollicitudin nibh sit amet commodo nulla. ")
-    lorem.append("Vulputate enim nulla aliquet porttitor lacus luctus. ")
-    lorem.append("Mattis pellentesque id nibh tortor id aliquet.\n")
-    lorem.append("\nTurpis cursus in hac habitasse platea. ")
-    lorem.append("Semper risus in hendrerit gravida rutrum quisque. ")
-    lorem.append("Pulvinar neque laoreet suspendisse interdum.\n")
-    lorem.append("\nIn est ante in nibh mauris. ")
-    
-    random.shuffle(lorem)
-    
-    lorem_out = lorem[:random.randint(1, len(lorem))]
-    lorem_addendum = ''.join(lorem_out)
-    response_text = dump_text + lorem_addendum
-
-    for i in range(0, len(response_text), 5):
-        chunk = response_text[i:i+5]
-        # await asyncio.sleep(0.01) # 0.02 : 33 t/s .  0.01 : 66 t/s . 0.0 : 960 t/s . commented  : 1030 t / s
-        await websocket.send_text(chunk)
-
-    await websocket.close()
-
-
-app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 
 @app.get("/get-items-active")
 def get_items_active():
@@ -115,4 +78,3 @@ def get_functions():
     for func_wrapper in function_wrapper.functions.values():
         functions_info.append(function_wrapper.FunctionInfo(friendly_name=func_wrapper.friendly_name, id=func_wrapper.id))
     return functions_info
-
